ac210aa54081a8e21e5521fa097302273c610c7d
root <root@c1136-node2.coelab.cloudera.com>
2021-04-09 17:17:00 +0000

    Update to make it comply with CDP 7.2.7

--- hwc_info_collect.sh
+++ hwc_info_collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-#### For version: CDP 7.2.2 Private Cloud Base
+#### For version: CDP 7.2.7 Private Cloud Base
 
 ### Notes:
 
@@ -33,65 +33,60 @@ beeline_site=/etc/hive/conf.cloudera.hive_on_tez/beeline-site.xml
 if [ -r "$hive_site" ] && [ -r "$beeline_site" ]; then
 
     hive_metastore_uris=$(grep -e "thrift.*9083" "$hive_site" |awk -F"<|>" '{print $3}')
-    hive_llap_daemon_service_hosts=$(grep "hive.llap.daemon.service.hosts" -A1 "$hive_site" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
     hive_zookeeper_quorum=$(grep "hive.zookeeper.quorum" -A1 "$hive_site" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
     hwc_jar=$(find /opt/cloudera/parcels/CDH/lib/ -name *hive-warehouse-connector*assembly*.jar)
     hwc_pyfile=$(find /opt/cloudera/parcels/CDH/lib/ -name pyspark_hwc-*.zip)
     hive_jdbc_url=$(grep "jdbc:hive2" "$beeline_site" -A1 | awk 'NR==1' | awk -F"[<|>]" '{print $3}')
     hive_jdbc_url_principal=$(grep "hive.server2.authentication.kerberos.principal" -A1 "$hive_site" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
-    hive_zookeeper_quorum=$(grep "hive.zookeeper.quorum" -A1 "$hive_site" |awk 'NR==2' | awk -F"[<|>]" '{print $3}')
-    hms_uri=$(grep uri /etc/hive/conf/hive-site.xml -A1  |awk 'NR==2' | awk -F"[<|>]" '{print $3}')    
 
-    echo -e "To apply this configuration cluster wide, copy and paste the following list of properties in CM UI -> Spark -> Configuration -> Spark Client Advanced Configuration Snippet (Safety Valve) for spark-conf/spark-defaults.conf\n"
-    echo -e "These are the recommended properties for a production environment.\n\n"
+    echo -e "\nTo apply these configurations cluster wide, copy and paste the following list of properties in CM UI -> Spark -> Configuration -> Spark Client Advanced Configuration Snippet (Safety Valve) for spark-conf/spark-defaults.conf\n"
 
-	    echo -e "1st Option) AutoTranslate enabled: Use regular SPARK API, the reader/writer mode will be automatically selected based on the table's type..:\n"
-	    echo -e "spark.sql.extensions=com.qubole.spark.hiveacid.HiveAcidAutoConvertExtension"
-	    echo -e "spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator"
-	    
-	    echo -e "\n2nd Option) Read: SparkDirectReader, Write: HWC. \n"
-	    echo -e "spark.jars="$hwc_jar
-	    echo -e "spark.submit.pyFiles="$hwc_pyfile
-	    echo -e "spark.sql.hive.hiveserver2.jdbc.url="$hive_jdbc_url
-	    echo -e "spark.sql.hive.zookeeper.quorum="$hive_zookeeper_quorum
-	    echo -e "spark.hadoop.hive.metastore.uris="$hms_uri
-	    echo -e "spark.sql.hive.hwc.execution.mode=spark"
+    echo -e "spark.hadoop.hive.metastore.uris="$hive_metastore_uris
+    echo -e "spark.hadoop.hive.zookeeper.quorum="$hive_zookeeper_quorum
+    echo -e "spark.jars="$hwc_jar
+    echo -e "spark.submit.pyFiles="$hwc_pyfile
+    echo -e "spark.sql.hive.hiveserver2.jdbc.url="$hive_jdbc_url
+    [ ! -z "$hive_jdbc_url_principal"] && echo -e "spark.sql.hive.hiveserver2.jdbc.url.principal="$hive_jdbc_url_principal
 
-	    echo -e "\n3rd Option) Read: JDBC, Write: HWC.\n"
-	    echo -e "spark.jars="$hwc_jar
-	    echo -e "spark.submit.pyFiles="$hwc_pyfile
-	    echo -e "spark.sql.hive.hiveserver2.jdbc.url="$hive_jdbc_url
-	    echo -e "spark.hadoop.hive.zookeeper.quorum="$hive_zookeeper_quorum
-	    echo -e "spark.hadoop.hive.metastore.uris="$hms_uri
-	    
-
-	    #If Kerberized:
-	    [ ! -z "$hive_jdbc_url_principal"] && echo -e "spark.sql.hive.hiveserver2.jdbc.url.principal="$hive_jdbc_url_principal
-
-	    echo -e "\n### Save and restart."
-	    echo -e "\nNote: In a kerberized environment the property spark.security.credentials.hiveserver2.enabled has to be set to TRUE for deploy-mode cluster, i.e.:\n spark-submit --conf spark.security.credentials.hiveserver2.enabled=true"
 
 	    echo -e "\nIf you'd like to test this per job instead of cluster wide, then use the following command as an example:\n
 
-            AutoTranslate:
-	    spark-shell --master yarn --conf spark.datasource.hive.warehouse.load.staging.dir=/tmp --conf spark.datasource.hive.warehouse.metastoreUri=$hive_metastore_uris --conf spark.hadoop.hive.llap.daemon.service.hosts=$hive_llap_daemon_service_hosts --conf spark.jars=$hwc_jar --conf spark.submit.pyFiles=$hwc_pyfile --conf spark.security.credentials.hiveserver2.enabled=false --conf spark.sql.hive.hiveserver2.jdbc.url=\"$hive_jdbc_url\" --conf spark.sql.hive.zookeeper.quorum=\"$hive_zookeeper_quorum\" \n
+            To spin up a Spark-Shell with Spark Direct Reader mode::
 
-            SparkDirectReader:
+	    spark-shell --master yarn \\
+            --conf spark.jars=$hwc_jar \\
+            --conf spark.sql.extensions=com.hortonworks.spark.sql.rule.Extensions \\
+            --conf spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator \\
+            --conf spark.submit.pyFiles=$hwc_pyfile \\
+            --conf spark.sql.hive.hiveserver2.jdbc.url=$hive_jdbc_url \\
+            --conf spark.hadoop.hive.metastore.uris=$hive_metastore_uris \\ 
+            --conf spark.datasource.hive.warehouse.read.mode=DIRECT_READER_V1
+	    "
+            [ ! -z "$hive_jdbc_url_principal"] && echo -e "\\ --conf spark.sql.hive.hiveserver2.jdbc.url.principal="$hive_jdbc_url_principal
+            
 
-	    spark-shell --master yarn --conf spark.datasource.hive.warehouse.load.staging.dir=/tmp --conf spark.datasource.hive.warehouse.metastoreUri=$hive_metastore_uris --conf spark.hadoop.hive.llap.daemon.service.hosts=$hive_llap_daemon_service_hosts --conf spark.jars=$hwc_jar --conf spark.submit.pyFiles=$hwc_pyfile --conf spark.security.credentials.hiveserver2.enabled=false --conf spark.sql.hive.hiveserver2.jdbc.url=\"$hive_jdbc_url\" --conf spark.sql.hive.zookeeper.quorum=\"$hive_zookeeper_quorum\" \n
+            echo -e "\nHow to spin up a Spark-Shell with JDBC Cluster/Executors mode (Client/Driver mode is not recommended)::
 
-            JDBC Mode::
+	    spark-shell --master yarn \\
+            --conf spark.jars=$hwc_jar \\
+            --conf spark.sql.extensions=\"com.hortonworks.spark.sql.rule.Extensions\" \\
+            --conf spark.kryo.registrator=com.qubole.spark.hiveacid.util.HiveAcidKyroRegistrator \\
+            --conf spark.submit.pyFiles=$hwc_pyfile \\
+            --conf spark.sql.hive.hiveserver2.jdbc.url=\"$hive_jdbc_url\"
+            --conf spark.hadoop.hive.zookeeper.quorum=$hive_zookeeper_quorum \\
+            --conf spark.datasource.hive.warehouse.read.mode=JDBC_CLUSTER"
+	    [ ! -z "$hive_jdbc_url_principal"] && echo -e "\\ --conf spark.sql.hive.hiveserver2.jdbc.url.principal="$hive_jdbc_url_principal
 
-	    spark-shell --master yarn --conf spark.datasource.hive.warehouse.load.staging.dir=/tmp --conf spark.datasource.hive.warehouse.metastoreUri=$hive_metastore_uris --conf spark.hadoop.hive.llap.daemon.service.hosts=$hive_llap_daemon_service_hosts --conf spark.jars=$hwc_jar --conf spark.submit.pyFiles=$hwc_pyfile --conf spark.security.credentials.hiveserver2.enabled=false --conf spark.sql.hive.hiveserver2.jdbc.url=\"$hive_jdbc_url\" --conf spark.sql.hive.zookeeper.quorum=\"$hive_zookeeper_quorum\" \n
-            "
-
-	    echo -e "Once in the Scala REPL, run the following snippet example to test basic conectivity:\n"
+	    echo -e "\nOnce in the Scala REPL, run the following snippet code example to test basic conectivity:\n"
 	    echo -e "scala> import com.hortonworks.hwc.HiveWarehouseSession"
-	    echo "scala> import com.hortonworks.hwc.HiveWarehouseSession._"
-	    echo "scala> val hive = HiveWarehouseSession.session(spark).build()"
+	    echo -e "scala> import com.hortonworks.hwc.HiveWarehouseSession._"
+	    echo -e "scala> val hive = HiveWarehouseSession.session(spark).build()"
 	    echo -e "scala> hive.showDatabases().show()\n"
+	    
+            echo -e "\nNote: In a kerberized environment the property spark.security.credentials.hiveserver2.enabled has to be set to TRUE for Cluster mode, and FALSE for Client mode, i.e.:\n spark-submit --conf spark.security.credentials.hiveserver2.enabled=true/false"
+
 
 	else
 	     echo -e $hive_site" and/or "$beeline_site" doesn't exist on this host, or the current user $(whoami) doesn't have access to the files\n"
-     echo "Try running this command as the root or hive user"
+             echo "Try running this command as the root or hive user"
 fi
